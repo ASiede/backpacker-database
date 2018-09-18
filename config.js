f691d17fb51f694613be9b22310f67268f9daa49
Andrea Siede <andrea.siede@gmail.com>
2018-09-18 10:47:20 -0700

    changed to mlab dbs

--- config.js
+++ config.js
@@ -1,8 +1,13 @@
 "use strict";
+// exports.DATABASE_URL =
+//   process.env.DATABASE_URL || "mongodb://localhost/backpacking-app";
+
 exports.DATABASE_URL =
-  process.env.DATABASE_URL || "mongodb://localhost/backpacking-app";
+  process.env.DATABASE_URL || "mongodb://asiede:SashaB00ne@ds155862.mlab.com:55862/backpacking-app";
+// exports.TEST_DATABASE_URL =
+//   process.env.TEST_DATABASE_URL || "mongodb://localhost/test-backpacking-app";
 
 exports.TEST_DATABASE_URL =
-  process.env.TEST_DATABASE_URL || "mongodb://localhost/test-backpacking-app";
+ process.env.TEST_DATABASE_URL || "mongodb://asiede:SashaB00ne@ds245210.mlab.com:45210/test-backpacking-app";
 
 exports.PORT = process.env.PORT || 8080;
